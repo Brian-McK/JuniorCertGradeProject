936bbb124350eeb4c4c919af4df31c1f9144d307
Brian McKenna <D00197352@student.dkit.ie>
2020-10-24 17:32:31 +0100

    selectFiveGrades throwing an error

--- src/main/java/com/dkit/sd2b/BrianMcKenna/Main.java
+++ src/main/java/com/dkit/sd2b/BrianMcKenna/Main.java
@@ -2,6 +2,8 @@ package com.dkit.sd2b.BrianMcKenna;
 
 /* Brian McKenna - SD2B - D00197352 */
 
+/* GitHub Repository Link - https://github.com/Brian-McK/JuniorCertGradeProject */
+
 /*
  The program should output a list of student numbers and their corresponding calculated average value. e.g. 891234 56.54
  Each line represents one student record.
@@ -12,51 +14,58 @@ package com.dkit.sd2b.BrianMcKenna;
 */
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main
 {
-    public static void main( String[] args ) throws FileNotFoundException
+    public static void main( String[] args )
     {
-        File jcResults = new File("JC_Results.txt");
-        Scanner scan = new Scanner(jcResults).useDelimiter("[,\r\n]+");
-
-        int [] subjectCodes = {1,2,3,4,5,6,7,8,9,10,11,12,13,27,40,41,42,46,47,48,52,54,57,65,81,82,83,100,106,125,
-                126,137,200,217,218,220,221,224,565,569};
-
-        int maxSubjects = 8;
-
-        // TODO: 21/10/2020 - add try / catch 
-        // TODO: 21/10/2020 - make a method for the file 
-        // TODO: 21/10/2020 - add more records to the results file
-
-        while(scan.hasNext())
-        {
-            int studentNumber = scan.nextInt();
-            int [] subjectCodeResults = new int[maxSubjects];
-            int [] subjectResults = new int[maxSubjects];
-
-            int i = 0;
-            while (i < maxSubjects)
-            {
-                int subjectCode = scan.nextInt();
-                subjectCodeResults[i] = subjectCode;
-
-                int result = scan.nextInt();
-                subjectResults[i] = result;
-
-                i++;
-            }
-
-            int [] top5Grades = selectFiveGrades(subjectCodeResults,subjectResults);
-            double averageGrade = calculateAverage(top5Grades);
-
-            printStudentResult(studentNumber,averageGrade);
-        }
-        scan.close();
+        readTxtFile("JC_Results.txt");
     }
+
+    public static void readTxtFile(String pathname)
+    {
+        try {
+            File jcResults = new File(pathname);
+            Scanner scan = new Scanner(jcResults).useDelimiter("[,\r\n]+");
+            int maxSubjects = 8;
+
+            // TODO: 21/10/2020 - add more records to the results file
+
+            while(scan.hasNext())
+            {
+                int studentNumber = scan.nextInt();
+                int [] subjectCodeResults = new int[maxSubjects];
+                int [] subjectResults = new int[maxSubjects];
+
+                int i = 0;
+                while (i < maxSubjects)
+                {
+                    int subjectCode = scan.nextInt();
+                    subjectCodeResults[i] = subjectCode;
+
+                    int result = scan.nextInt();
+                    subjectResults[i] = result;
+
+                    i++;
+                }
+
+                int [] top5Grades = selectFiveGrades(subjectCodeResults,subjectResults);
+                double averageGrade = calculateAverage(top5Grades);
+
+                printStudentResult(studentNumber,averageGrade);
+            }
+            scan.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error - File not found, " + e.getMessage());
+        }
+    }
+
+    // TODO: 24/10/2020 - fix logic below - not working properly - last record in txt file throwing an error
 
     public static int[] selectFiveGrades(int[] codes, int[] allGrades)
     {
@@ -89,7 +98,6 @@ public class Main
         }
 
         Arrays.sort(temp);
-
         top5grades[top5grades.length - 1] = temp[temp.length - 1];
         top5grades[top5grades.length - 2] = temp[temp.length - 2];
 
